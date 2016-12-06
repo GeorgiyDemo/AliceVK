b6db4eb8701632284a5fcf04266161bad17e3d1e
Georgiy Demenchuk <demenchuk.george@mail.ru>
2016-12-06 17:12:15 +0300

    Democracy, lol

--- Alice.py
+++ Alice.py
@@ -124,6 +124,9 @@ def get_weather():
 #Счетчик дней до Нового Года
 a = '2017-01-01'.split('-')
 aa = datetime.date(int(a[0]),int(a[1]),int(a[2]))
+#
+afix = '2016-08-30'.split('-')
+aafix = datetime.date(int(afix[0]),int(afix[1]),int(afix[2]))
 
 while True:
 
@@ -140,6 +143,9 @@ while True:
     #Счетчик дней учебы
     cc = aa-bb
     dd = int(str(cc).split()[0])
+    #
+    ccfix = bb-aafix
+    ddfix = int(str(ccfix).split()[0])
 
     #Работа с падежами числительных
     days =  print_(numeral.choose_plural(int(dd), (u'день', u'дня', u'дней')))
@@ -148,7 +154,7 @@ while True:
     chat_titles = {
     '1': '2ПКС-115 | До НГ '+ str(dd)+' '+days,
     '2':'II Курс | До НГ '+ str(dd)+' '+days,
-    '3':'I Курс | До НГ '+ str(dd)+' '+days,
+    '3':'I Курс | '+str(ddfix)+' день учебы',
     }
 
     #Чекаем названия бесед
